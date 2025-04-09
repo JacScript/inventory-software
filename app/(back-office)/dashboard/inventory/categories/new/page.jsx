4f81284870f8c0bb4808f546ede0ab655a1cd790
JacScript <jacksonsylas59@gmail.com>
2025-04-09 20:15:54 +0300

    minor changes

--- app/(back-office)/dashboard/inventory/categories/new/page.jsx
+++ app/(back-office)/dashboard/inventory/categories/new/page.jsx
@@ -1,28 +1,48 @@
+"use client"
 import FormHeader from '@/components/dashboard/FormHeader'
-import React from 'react'
+import SubmitButton from '@/components/FormInputs/SubmitButton'
+import TextInput from '@/components/FormInputs/TextInput'
+import { Plus } from 'lucide-react'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 export default function NewCategory() {
+  const [loading, setLoading] = useState(false)
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: {errors},
+  } = useForm()
+
+
+  function onSubmit(data) {
+    console.log(data);
+    reset()
+  }
+
   return (
     <div>
       {/* header */}
     <FormHeader title="New Category" href="#"/>
  
       {/* form */}
-      <form action="" className='w-full max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 mx-auto my-3'>
+      <form 
+      action=""
+      onSubmit={handleSubmit(onSubmit)}
+       className='w-full max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 mx-auto my-3'>
          <div className='grid gap-4 sm:gap-6 sm:grid-cols-2'>
-         <div className="sm:col-span-2">
-                  <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Name</label>
-                  <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required=""/>
-              </div>
-              <div className="w-full">
-                  <label for="brand" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Brand</label>
-                  <input type="text" name="brand" id="brand" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Product brand" required="" />
-              </div>
+         <TextInput 
+           label="Category Title"
+           name="title"
+          //  isRequired ={true}
+           register={register}
+           errors={errors}
+          //  type= "text"
+          //  className="sm:col-span-2"
+         />
          </div>
-         <div className='mt-6 sm:col-span-1'>
-         <button type="submit" className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
-
-         </div>
+         <SubmitButton isLoading={loading} title="Category"/>
       </form>
     </div>
   )
