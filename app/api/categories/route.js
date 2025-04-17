6a58feb797cc03c3b77fb8d2f8670e18b367185c
JacScript <jacksonsylas59@gmail.com>
2025-04-17 15:28:33 +0300

    prisma setup

--- app/api/categories/route.js
+++ app/api/categories/route.js
@@ -1,36 +1,16 @@
-// import { NextResponse } from "next/server";
-
-// export async function POST(request){
-//    try {
-    
-
-//     const {title, description } = await request.json();
-//     const category = {title, description}
-//     console.log(category);
-//     return NextResponse.json(category);
-
-
-//    } catch (error) {
-//     console.log(error )
-//      return NextResponse.json({
-//         error,
-//         message: "Failed to create a category"
-//      },{
-//         status: 500
-//      })
-//    }
-// }
-
-
-
-
+import db from "@/lib/db";
 import { NextResponse } from "next/server";
 
 // Handle POST request
 export async function POST(request) {
   try {
     const { title, description } = await request.json();
-    const category = { title, description };
+    const category = await db.category.create({
+      data:{
+        title,
+        description
+      }
+    })
     console.log(category);
 
     return NextResponse.json(category, {
